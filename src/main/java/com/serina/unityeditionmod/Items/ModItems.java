460371bff55a41d047f6fec60934786035b99c51
Serina <sovietserina53@gmail.com>
2026-08-18 12:13:27 -0500

    finished ordering the project and added the block and item registers

---
+++ src/main/java/com/serina/unityeditionmod/Items/ModItems.java
@@ -0,0 +1,17 @@
+package com.serina.unityeditionmod.Items;
+
+import com.serina.unityeditionmod.Config.SerinasWorldUnityEdition;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS=DeferredRegister.createItems(SerinasWorldUnityEdition.MODID);
+
+
+
+
+    public static void register(IEventBus eventBus){ITEMS.register(eventBus);}
+}
